be87ff36e4d6ca1f5c39978fd032718c5ccf5e07
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-02-07 12:38:27 -0600

    Add debug logging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,17 +14,20 @@ class StaticPagesController < ApplicationController
     # Handle workshop key provided via URL params first
     if params[:id]
       user_from_workshop_key = User.find_by_workshop_key(params[:id])
-      if user_from_workshop_key
-        session[:user_id] = user_from_workshop_key.id
-        return redirect_to workshop_path
-      else
+      if user_from_workshop_key.nil?
         reset_session
         flash.now[:notice] = "Sorry, but we couldn't find your workshop! Please follow the link from your email or enter your information on the signup form."
         return render "users/new_workshop_user"
       end
+      logger.debug "Setting user id in session to #{user_from_workshop_key.id}"
+      session[:user_id] = user_from_workshop_key.id
+      return redirect_to workshop_path
     end
 
-    return render "users/new_workshop_user" unless @current_user
+    unless @current_user
+      logger.debug "Couldn't find current user with id #{session[:user_id]}"
+      return render "users/new_workshop_user"
+    end
 
     @current_user.increment!(:workshop_page_views)
   end
