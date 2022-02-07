dfc37b7581a902e782b5b49e59ea91c61f100660
Joshua Hunsche Jones <joshua@hunschejones.com>
2022-02-07 12:53:30 -0600

    Add more debug logging

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,13 +19,14 @@ class StaticPagesController < ApplicationController
         flash.now[:notice] = "Sorry, but we couldn't find your workshop! Please follow the link from your email or enter your information on the signup form."
         return render "users/new_workshop_user"
       end
-      logger.info "Setting user id in session to #{user_from_workshop_key.id}"
+      logger.debug "Setting user id in session to '#{user_from_workshop_key.id}'"
       session[:user_id] = user_from_workshop_key.id
+      logger.debug "User id set in session to '#{session[:user_id]}'"
       return redirect_to workshop_path, status: :see_other
     end
 
     if @current_user.nil?
-      logger.info "Couldn't find current user with id #{session[:user_id]}"
+      logger.debug "Couldn't find current user with id '#{session[:user_id]}'"
       return render "users/new_workshop_user"
     end
 
